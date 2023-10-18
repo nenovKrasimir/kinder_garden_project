3473bd6c85692b478323fab0c51d2b1d17d06f44
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-10-18 18:55:13 +0300

    working on appearance, added new page, added script

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('za_nas', views.about_us, name='about_us')
 ]
---
+++ static/js/scripts.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Remove the loading overlay once the page has fully loaded
+    var loadingOverlay = document.getElementById("loading-overlay");
+    loadingOverlay.style.display = "none";
+});
