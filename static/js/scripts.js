fd2551f532d402a2a1c896f2dcbbc2fc0a776acc
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-10-18 23:58:18 +0300

    working on about-us animations and media

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,5 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Remove the loading overlay once the page has fully loaded
-    var loadingOverlay = document.getElementById("loading-overlay");
-    loadingOverlay.style.display = "none";
-});
+const paragraphs = document.querySelectorAll('.animated-paragraph');
+let currentIndex = 0;
+
+function animateParagraphs() {
+    if (currentIndex < paragraphs.length) {
+        setTimeout(() => {
+            paragraphs[currentIndex].style.opacity = 1; // Make the paragraph visible
+            currentIndex++;
+            animateParagraphs(); // Move to the next paragraph with a delay
+        }, 1800); // Delay before showing the next paragraph
+    }
+}
+
+animateParagraphs(); // Start showing and animating paragraphs one by one
