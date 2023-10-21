08fe95820a259c0287f31f2ab8754dca741c2a04
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-10-21 13:33:49 +0300

    contact page created

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -14,3 +14,12 @@ function animateParagraphs() {
 animateParagraphs(); // Start showing and animating paragraphs one by one
 
 
+// Get the current URL
+const currentURL = window.location.pathname;
+
+// Select the link based on the current URL and add the "active" class
+document.querySelectorAll('nav a').forEach(link => {
+    if (link.getAttribute('href') === currentURL) {
+        link.classList.add('active');
+    }
+});
